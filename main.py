61ee3e24d8aab1af2a84687aadf4d05bd0a331c9
Leiner117 <lealvarado@estudiantec.cr>
2022-04-19 21:16:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import functions_admins
+import registro
+
 def login():
     while True:
         print("1. Login\n2. Registro\n3. salir")
@@ -14,19 +16,35 @@ def register():
     print("Desea registrarse como: \n1. Administrativo\n2. Estudiante")
     opselect = int(input("--->"))
     if (opselect == 1):
-        functions_admins.add_admins
+        functions_admins.add_admins()
         login()
     elif (opselect == 2):
-        print()
+        registro.register()
 def menu_admins():
-    print("Menú de administradores")
-    print("1. Agregar cursos\n2. Modificar cursos\n3. Agregar carreras\n4. Modificar carreras")
-    opselect = int(input("Ingrese la opcion que desea: "))
-    if (opselect == 1):
-        functions_admins.add_courses
-    elif (opselect == 2):
-        functions_admins.mod_courses
-    elif (opselect == 3):
-        functions_admins.add_careers
-    elif (opselect == 4):
-        print()
+    while True:
+        print("Menú de administradores")
+        print("1. Agregar cursos\n2. Modificar cursos\n3. Agregar carreras\n4. Modificar carreras")
+        opselect = int(input("Ingrese la opcion que desea: "))
+        if (opselect == 1):
+            functions_admins.add_courses()
+        elif (opselect == 2):
+            functions_admins.mod_courses()
+        elif (opselect == 3):
+            functions_admins.add_careers()
+        elif (opselect == 4):
+            print()
+        
+def menu_students():
+    while True:
+        print("Menú de estudiantes")
+        print("1. Modificar carreras \n2. \n3. Agregar carreras\n4. Modificar carreras")
+        opselect = int(input("Ingrese la opcion que desea: "))
+        if (opselect == 1):
+            functions_admins.add_courses()
+        elif (opselect == 2):
+            functions_admins.mod_courses()
+        elif (opselect == 3):
+            functions_admins.add_careers()
+        elif (opselect == 4):
+            print()
+register()
