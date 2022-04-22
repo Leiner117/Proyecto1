e09a6bb40638032a61e8ee4411e9229d1df113e3
Leiner117 <lealvarado@estudiantec.cr>
2022-04-21 19:20:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import functions_admins
 import functions_students
 def login():
     while True:
-        print("1. Login\n2. Registro\n3. salir")
+        print("1. Login\n2. Registro\n3. salir\n4. menu admin")
         opselect = int(input("Seleccione la opcion que desea: "))
         if (opselect == 1):
-            print()
+            menu_students()
         elif (opselect == 2):
             register()
         elif (opselect == 3):
             break
+        elif (opselect == 4):
+            menu_admins()
  
 def register():
     print("Desea registrarse como: \n1. Administrativo\n2. Estudiante")
@@ -19,10 +21,11 @@ def register():
         login()
     elif (opselect == 2):
         functions_students.register()
+        login()
 def menu_admins():
     while True:
         print("Menú de administradores")
-        print("1. Agregar cursos\n2. Modificar cursos\n3. Agregar carreras\n4. Modificar carreras")
+        print("1. Agregar cursos\n2. Modificar cursos\n3. Agregar carreras\n4. Modificar carreras\n5. Salir")
         opselect = int(input("Ingrese la opcion que desea: "))
         if (opselect == 1):
             functions_admins.add_courses()
@@ -36,17 +39,22 @@ def menu_admins():
             print(functions_admins.courses)
             print(functions_admins.careers)
             print(functions_admins.admins)
+        elif (opselect == 6):
+            break
         
 def menu_students():
     while True:
         print("Menú de estudiantes")
-        print("1. Modificar carreras \n2. \n3. Agregar carreras\n4. Modificar carreras")
+        print("1. asignar cursos \n2. \n3. Agregar carreras\n4. Modificar carreras")
         opselect = int(input("Ingrese la opcion que desea: "))
         if (opselect == 1):
-            functions_admins.add_courses()
+            functions_students.assign_course("leiner")
         elif (opselect == 2):
             functions_admins.mod_courses()
         elif (opselect == 3):
             functions_admins.add_careers()
         elif (opselect == 4):
             print()
+menu_admins()
+
+    
