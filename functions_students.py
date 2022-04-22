b59dfa7fc4120089ac52102753f5ef0b05eba4ee
Leiner117 <lealvarado@estudiantec.cr>
2022-04-21 19:20:13 -0600

    Update functions_students.py

--- functions_students.py
+++ functions_students.py
@@ -1,7 +1,8 @@
 import functions_admins
-students = []
+students = [["leiner","","inge","",[]]]
 def register (): 
     auxlist = []
+    courses = []
     name = input("Ingrese su nombre: ")
     email =  input("porfavor introduce tu gmail: ")   
     course = functions_admins.select_position_careers()
@@ -11,10 +12,43 @@ def register ():
     auxlist.append(email)
     auxlist.append(course)
     auxlist.append(password)
+    auxlist.append(courses)
     students.append(auxlist)
     print("Te has registrado con exito!!! ")
     print(students)
+def mod_careers(name):
+    new_career = functions_admins.select_position_careers()
+    new_career = functions_admins.careers[new_career]
+    for i in students:
+        if name in i:
+            i[2] = new_career
+            
+def assign_course(name):
+    career = ""
+    b = 0
+    for i in students:
+        if name in i:
+            career = i[2]
+            index = students.index(i)
+            break
+    print("Seleccione el curso que desea:")
+    for a in functions_admins.courses:
+        if career in a[6]:
+            b = b+1
+            print(str(b)+"-"+a[0])
+    select = int(input("---> "))
+    students[index][4].append(functions_admins.courses[select-1][0])
+    print(students)
 
+            
+                
+           
+                    
+            
+            
+
+            
+    
     
     
     
