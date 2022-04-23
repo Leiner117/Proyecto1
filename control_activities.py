dc10393803bcc9c00a9ce53b3aa759e7eec13f91
Leiner117 <lealvarado@estudiantec.cr>
2022-04-23 16:38:57 -0600

    Create control_activities.py

---
+++ control_activities.py
@@ -0,0 +1,9 @@
+import functions_admins
+import functions_students
+from datetime import datetime,time
+import calendar
+schedule = []
+def record_attendance():
+    
+    functions_students.students
+    schedule
