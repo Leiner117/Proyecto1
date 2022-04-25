29daebe5e3044e8541508659aa42008ffe20f5b9
josue328 <71100469+josue328@users.noreply.github.com>
2022-04-24 22:45:28 -0600

    Create functions_encript.py

---
+++ functions_encript.py
@@ -0,0 +1,37 @@
+import functions_admins
+from datetime import datetime,time
+from cryptography.fernet import Fernet 
+
+students = [["leiner","","inge","12345",[{'curso':'mate','estado':'Aprobado'},{'curso':'progra','estado':'En curso'}],[]]]
+def register (): 
+    auxlist = []
+    courses = []
+    activities = []
+    name = input("Ingrese su nombre: ")
+    email =  input("porfavor introduce tu gmail: ")   
+    course = functions_admins.select_position_careers()
+    course = functions_admins.careers[course]
+    password = input("porfavor introduce una contraseña: ")
+    
+    
+    key = Fernet.generate_key()
+    objeto_cifrado = Fernet(key)
+    texto_encriptado = objeto_cifrado.encrypt(str.encode(password))
+
+    texto_desencriptado_bytes = objeto_cifrado.decrypt(texto_encriptado)
+    texto_desencriptado = texto_desencriptado_bytes.decode()
+  
+    
+    
+    
+    auxlist.append(name)
+    auxlist.append(email)
+    auxlist.append(course)
+    auxlist.append(texto_encriptado)
+    auxlist.append(courses)
+    auxlist.append(activities)
+    students.append(auxlist)
+    print("Te has registrado con exito!!! ")
+    print(students)
+    
+register()
