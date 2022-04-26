a23eef971773420eb7a60839bc54dc3a3cde4624
Leiner117 <lealvarado@estudiantec.cr>
2022-04-26 00:24:31 -0600

    Update reports.py

--- reports.py
+++ reports.py
@@ -1,16 +1,22 @@
 import functions_students
+from datetime import datetime
 def print_activities(name,date):
+    '''
+    recorre el diccionario de actividades
+    verifica que las fechas sean iguales
+    imprime la informacion de la fecha 
+    '''
     index = functions_students.index_student(name)
     for i in functions_students.students[index][5]:
         if functions_students.students[index][5][i]["Fecha"] == date:
             if "descripcion" in functions_students.students[index][5][i]:
                 print("Día:"+str(functions_students.students[index][5][i]["Fecha"]))
-                print("Actividad:"+functions_students.students[index][5][i]["Descripcion"])
+                print("Actividad:"+functions_students.students[index][5][i]["descripcion"])
                 print("Hora de inicio:"+functions_students.students[index][5][i]["Hora inicio"])
                 print("Hora de conclusion:"+functions_students.students[index][5][i]["Hora conclusion"])
                 print("Curso asociado:"+functions_students.students[index][5][i]["curso"])
-                print("Total de tiempo:"+(functions_students.students[index][5][i]["Hora inicio"]-functions_students.students[index][5][i]["Hora conclusion"]))
+                print("Total de tiempo:"+(datetime.strptime(functions_students.students[index][5][i]["Hora inicio"], '%H:%M')-datetime.strptime(functions_students.students[index][5][i]["Hora conclusion"], '%H:%M')))
+                break
                 
-                
-        
+
     
